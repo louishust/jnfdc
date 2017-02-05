f946ac8ddafe5447b19bb0f39d3d65ebc8eb9a65
Shuai Lou <shuai.lou@turbolinux.com.cn>
2017-02-05 20:12:47 +0800

    modify fetch pos

--- app/util/fetch.py
+++ app/util/fetch.py
@@ -6,7 +6,7 @@ from datetime import date
 def fetch_jnfdc():
     page = requests.get('http://www.jnfdc.gov.cn/')
     tree = html.fromstring(page.content)
-    sn = tree.xpath('//div[@id="todayview"]/div[@class="col_bg"]/ul[1]/ul[2]/li[2]/text()')[0]
+    sn = tree.xpath('//div[@id="todayview"]/div[@class="col_bg"]/ul[1]/ul[1]/li[2]/text()')[0]
     netsign = NetSign.query.filter_by(date=date.today()).first()
     if netsign is None:
         netsign = NetSign(signnum = int(sn), date = date.today())
