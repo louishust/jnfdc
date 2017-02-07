79c3ab04b8e9f70bfde3e008987801bd0c69851e
Shuai Lou <shuai.lou@turbolinux.com.cn>
2017-02-07 11:00:20 +0800

    add try catch

--- app/util/fetch.py
+++ app/util/fetch.py
@@ -4,15 +4,18 @@ from ..models import NetSign
 from datetime import date
 
 def fetch_jnfdc():
-    page = requests.get('http://www.jnfdc.gov.cn/')
-    tree = html.fromstring(page.content)
-    sn = tree.xpath('//div[@id="todayview"]/div[@class="col_bg"]/ul[1]/ul[1]/li[2]/text()')[0]
-    netsign = NetSign.query.filter_by(date=date.today()).first()
-    if netsign is None:
-        netsign = NetSign(signnum = int(sn), date = date.today())
-    else:
-        netsign.signnum = int(sn)
-    netsign.add()
+    try:
+        page = requests.get('http://www.jnfdc.gov.cn/')
+        tree = html.fromstring(page.content)
+        sn = tree.xpath('//div[@id="todayview"]/div[@class="col_bg"]/ul[1]/ul[1]/li[2]/text()')[0]
+        netsign = NetSign.query.filter_by(date=date.today()).first()
+        if netsign is None:
+            netsign = NetSign(signnum = int(sn), date = date.today())
+        else:
+            netsign.signnum = int(sn)
+        netsign.add()
+    except Exception:
+        pass
 
 if __name__ == '__main__':
     parse_jnfdc()
